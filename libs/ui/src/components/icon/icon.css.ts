a39d275dc8cc7a2be45534db9fc9a41838c4f838
Eliezer Souza <eliezervinicius@outlook.com>
2022-11-12 18:08:24 -0300

    feat(icon): adding icon component (#6)

---
+++ libs/ui/src/components/icon/icon.css.ts
@@ -0,0 +1,28 @@
+import { recipe, RecipeVariants } from '@vanilla-extract/recipes';
+import { theme } from '../../theme/theme.css';
+
+const iconStyle = recipe({
+  base: {
+    color: 'inherit',
+  },
+
+  variants: {
+    primary: {
+        true: { color: theme.color.primary.main },
+    },
+    sizes: {
+      xs: { fontSize: '0.75rem', lineHeight: '1rem' },
+      sm: { fontSize: '0.875rem', lineHeight: '1.25rem' },
+      md: { fontSize: '1rem', lineHeight: '1.5rem' },
+      lg: { fontSize: '1.125rem', lineHeight: '1.75rem' },
+    },
+  },
+
+  defaultVariants: {
+    primary: true,
+    sizes: 'md'
+  }
+});
+
+export const icon = (name: string, style?: RecipeVariants<typeof iconStyle>) =>
+  `${name} ${iconStyle(style)}`;

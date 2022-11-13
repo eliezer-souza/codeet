17954cade9fd79ad588dbba8ec41b2cf3909c296
Eliezer Souza <eliezervinicius@outlook.com>
2022-11-13 16:28:27 -0300

    feat(tailwind): adding tailwind in project (#11)

---
+++ apps/codeet-next/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <h1 className="text-3xl font-bold underline text-primary">
+      Hello, Next.js!
+    </h1>
+  );
+}
--- apps/codeet-next/next.config.js
+++ apps/codeet-next/next.config.js
@@ -7,6 +7,9 @@ const { withNx } = require('@nrwl/next/plugins/with-nx');
  * @type {import('@nrwl/next/plugins/with-nx').WithNxOptions}
  **/
 const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
   nx: {
     // Set this to true if you would like to to use SVGR
     // See: https://github.com/gregberge/svgr

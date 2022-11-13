758afa88f792cdc0cba58036cd7823ff296f1d98
Eliezer Souza <eliezervinicius@outlook.com>
2022-11-13 16:27:31 -0300

    feat(tailwind): adding tailwind in project

---
+++ apps/codeet-next/app/head.tsx
@@ -0,0 +1,9 @@
+export default function Head() {
+  return (
+    <>
+      <title>Codeet</title>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="width=device-width" />
+    </>
+  );
+}
---
+++ apps/codeet-next/tailwind.config.js
@@ -0,0 +1,41 @@
+const { join } = require('path');
+const { createGlobPatternsForDependencies } = require('@nrwl/react/tailwind');
+const { colors } = require('tailwindcss/colors');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    join(
+      __dirname,
+      '{app,pages,components}/**/*!(*.stories|*.spec).{ts,tsx,html}'
+    ),
+    ...createGlobPatternsForDependencies(__dirname),
+  ],
+  theme: {
+    container: {
+      center: true,
+    },
+    fill: {
+      none: 'none',
+    },
+    stroke: (theme) => ({
+      white: theme('colors.white'),
+    }),
+    extend: {
+      colors: {
+        ...colors,
+        lightPrimary: '#2D0973',
+        primary: '#24075b',
+        darkPrimary: '#1B0543',
+        yellow: '#FFD302',
+        pink: '#FF0049',
+        magenta: '#AD01B9',
+        violet: '#5D00C2',
+        bluedark: '#331CF2',
+        blue: '#1143FC',
+        bluelight: '#0097F0',
+      },
+    },
+  },
+  plugins: [],
+};

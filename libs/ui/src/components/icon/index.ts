b8130d04ea9c320031ec4614d81ff0466a8556ad
Eliezer Souza <eliezervinicius@outlook.com>
2022-11-13 15:41:58 -0300

    fix(package): removing vanilla-extract (#10)

--- libs/ui/src/components/icon/index.ts
+++
@@ -1,5 +0,0 @@
-import { RecipeVariants } from "@vanilla-extract/recipes";
-import { icon_style } from "./icon.css";
-
-export const icon = (name: string, style?: RecipeVariants<typeof icon_style>) =>
-  `${name} ${icon_style(style)}`;
--- libs/ui/src/components/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './button/button.css';
-export * from './icon/icon.css';
-export * from './icon';
-export * from './input/input.css';

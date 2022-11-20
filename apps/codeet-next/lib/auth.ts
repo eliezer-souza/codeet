134f05f438d3f25854ac8ddf438d4bb96327bc8c
Eliezer Souza <eliezervinicius@outlook.com>
2022-11-20 01:26:16 -0300

    feat(schema): adding externalId (#35)

--- apps/codeet-next/lib/auth.ts
+++ apps/codeet-next/lib/auth.ts
@@ -3,19 +3,30 @@ import GoogleProvider from 'next-auth/providers/google';
 import GithubProvider from 'next-auth/providers/github';
 import { prismaClient } from '@codeet/domain';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import { v4 as uuidv4 } from 'uuid';
 
 export const nextAuthOptions: NextAuthOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      profile(profile) {
+        return {
+          id: uuidv4(),
+          externalId: profile.sub,
+          name: profile.name,
+          email: profile.email,
+          image: profile.picture,
+        };
+      },
     }),
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
       profile(profile) {
         return {
-          id: profile.id.toString(),
+          id: uuidv4(),
+          externalId: profile.id.toString(),
           name: profile.name || profile.login,
           email: profile.email,
           image: profile.avatar_url,

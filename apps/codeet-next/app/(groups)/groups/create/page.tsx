c349074d5924f0beab8d368e4d73e287cc121cf4
eliezer-souza <eliezervinicius@outlook.com>
2022-12-13 22:57:13 -0300

    feat(group): adding Create Group button

--- apps/codeet-next/app/(groups)/groups/create/page.tsx
+++ apps/codeet-next/app/(groups)/groups/create/page.tsx
@@ -1,9 +1,11 @@
 import GroupForm from '../../../../components/group-form';
+import Back from '../../../../components/ui/back';
 
 export default async function CreateGroups() {
   return (
     <section className="container flex items-center justify-center gap-6 p-8 md:py-12 lg:pt-24">
       <div className="flex flex-col items-start gap-6 md:max-w-[960px] w-full">
+        <Back />
         <h1 className="text-xl font-bold leading-[1.1] sm:text-2xl md:text-4xl">
           Create group
         </h1>
